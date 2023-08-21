55374b3e1d000995590be822bc5e96ac5e273877
shravanm683 <nitkshravan@gmail.com>
2023-08-21 11:53:56 +0530

    Commit changes

--- mytickets/ticketmodel/ticket_management/tickets/models.py
+++ mytickets/ticketmodel/ticket_management/tickets/models.py
@@ -20,24 +20,3 @@ class Tickets(models.Model):
     def __str__(self):
         return self.name
 
-from django.db import models
-from django.utils import timezone
-
-# class Tickets(models.Model):
-#     TICKET_TYPES = [
-#         ('Type1', 'Type 1'),
-#         ('Type2', 'Type 2'),
-#         ('Type3', 'Type 3'),
-#     ]
-class Tickets(models.Model):
-    TICKET_TYPES = (
-        ("ASAP", "ASAP"),
-        ("FUTURE", "FUTURE")
-    )
-    type = models.CharField(max_length=10, choices=TICKET_TYPES)
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
